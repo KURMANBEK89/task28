b2ee9774bcdf607d22d8c6b6c97d115393b228ff
kurmanbek89 <kumakary2@gmail.com>
2022-11-27 15:24:48 +0300

    r

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer[]mass ={1,2,3,4,5,6,7,8,9,10};
+        Optional<Integer>optional= Stream.of(mass).map(x->x%2==0?x:0).map(y->y*y).max((x, y)->Integer.compare(x,y));
+        System.out.println(optional);
     }
 }
